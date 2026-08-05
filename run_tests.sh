029a2cbad4a3e54086ed4dc946860c6e17299219
Manon Rousseau <manon.rousseau.external@cnes.fr>
2026-08-05 13:12:40 +0000

    change pixi activ

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,8 @@
 echo "Testing docker image {$1}..."
 
 # Install pytest on top of existing environment
-/srv/pixi/pixi add --pypi pytest --manifest-path ~/notebook
-/srv/pixi/pixi shell --manifest-path ~/notebook
+/srv/pixi/pixi add --pypi pytest --manifest-path /srv/pixi/notebook --environement /home/jovyian/vre_name
+/srv/pixi/pixi shell --manifest-path /srv/pixi/notebook --environement /home/jovyian/vre_name
 #python -m pip install pytest
 
 pytest -v tests/test_all.py tests/test_$1.py
