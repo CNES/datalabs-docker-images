4c8faa956ecafd85a42da7ae1cf0d43f2d93f4b4
Roussman <150775061+Roussman@users.noreply.github.com>
2026-09-03 11:42:47 +0200

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 # Usage: docker run -w /srv/test -v $PWD:/srv/test pangeodev/base-notebook:latest ./run_tests.sh base-notebook
 echo "Testing docker image {$1}..."
 
-# Install pytest on top of existing environment
+# Install pytest on top of existing  environment
 python -m pip install pytest
 
 pytest -v tests/test_all.py tests/test_$1.py
