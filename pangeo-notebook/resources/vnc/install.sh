8ea8e1912577e751f73ae7befee9c1cd818529fe
Manon Rousseau <manon.rousseau.external@cnes.fr>
2026-09-04 14:24:58 +0000

    remove clean source

--- pangeo-notebook/resources/vnc/install.sh
+++ pangeo-notebook/resources/vnc/install.sh
@@ -18,9 +18,6 @@ cp resources/vnc/* /opt
 # Customize Desktop
 mkdir -p /opt/vre/
 
-echo "deb http://archive.ubuntu.com/ubuntu noble main restricted universe multiverse" > /etc/apt/sources.list
-echo "deb http://archive.ubuntu.com/ubuntu noble-updates main restricted universe multiverse" >> /etc/apt/sources.list
-echo "deb http://security.ubuntu.com/ubuntu noble-security main restricted universe multiverse" >> /etc/apt/sources.list
 
 add-apt-repository ppa:mozillateam/ppa --yes
 apt-get update --quiet
