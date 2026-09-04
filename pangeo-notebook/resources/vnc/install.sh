83f50e3e988cbc53e6b471a015ded3f9ec400991
Manon Rousseau <manon.rousseau.external@cnes.fr>
2026-09-04 08:06:22 +0000

    clean firefox config

--- pangeo-notebook/resources/vnc/install.sh
+++ pangeo-notebook/resources/vnc/install.sh
@@ -31,7 +31,7 @@ wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt
     gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}' && \
     echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null && \
     echo 'Package: *\nPin: origin packages.mozilla.org\nPin-Priority: 1000' | tee /etc/apt/preferences.d/mozilla && \
-    apt-get update && apt-get install -y --no-install-recommends \
+    apt-get update --quiet && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
         libpci-dev \
         libcanberra-gtk3-module \
         libgles2-mesa-dev \
@@ -47,7 +47,7 @@ echo "------------ FIN Firefox -----------------"
 
 echo "------------ DEBUT others -----------------"
 apt-get update --quiet && \
-apt-get install --yes --quiet --no-install-recommends \
+DEBIAN_FRONTEND=noninteractive apt-get install --yes --quiet --no-install-recommends \
     dconf-cli \
     dbus-x11 \
     evince \
