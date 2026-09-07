0c2fa93ed823ebf02c84cc5526bc471fdbf9fb89
Roussman <150775061+Roussman@users.noreply.github.com>
2026-09-07 15:18:23 +0200

    Fix apt get (#19)
    * add repo for apt
    
    * remove clean source
    
    * add mamba activate
    
    * use fix_apt_get in dockerfile
    
    * mamba
    
    * change mamba
    
    * add deactivate mamba
    
    * clean

--- pytorch-notebook/resources/vnc/install.sh
+++ pytorch-notebook/resources/vnc/install.sh
@@ -47,6 +47,8 @@ wget 'https://sourceforge.net/projects/turbovnc/files/3.1/turbovnc_3.1_amd64.deb
 apt-get install -y -q ./turbovnc_3.1_amd64.deb
 rm ./turbovnc_3.1_amd64.deb
 ln -s /opt/TurboVNC/bin/* /usr/local/bin/
+eval "$(mamba shell hook --shell bash)"
+mamba activate ${NB_PYTHON_PREFIX}
 mamba install --quiet websockify
 cp resources/branding/desktop/wallpaper.png /opt/vre/wallpaper.png
 cp -r resources/branding/desktop/xfce-perchannel-xml /etc/xdg/xfce4/xfconf/
@@ -63,3 +65,5 @@ sed -i 's/^#*/#/' /etc/xdg/user-dirs.defaults
 sed -i 's/enabled=True/enabled=False/' /etc/xdg/user-dirs.conf
 
 chmod 664 /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/*
+
+mamba deactivate
